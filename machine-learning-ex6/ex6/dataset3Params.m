98c2195574a17a4bd1bc32b645d3d39dadd5ed14
Nilesh Hirani <niilesh3105@gmail.com>
2017-04-13 04:26:59 +0530

    Code to select optimal value of C and sigma using cross-validation set

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -9,7 +9,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 
 % You need to return the following variables correctly.
 C = 1;
-sigma = 0.3;
+sigma = 0.1;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -24,9 +24,27 @@ sigma = 0.3;
 %
 
 
+% A = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+% err = zeros(8,8);
+% mini = [0 0];
+% minv = 10^9;
 
+% for i = [1:8]
+%     for j = [1: 8]
+%         C = A(i); sigma = A(j);
+%         model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+%         predictions = svmPredict(model, Xval);
+%         err(i,j) = mean(double(predictions ~= yval));
+%         if(err(i,j) < minv)
+%             minv = err(i,j);
+%             mini = [i j];
+%         end
+%     end
+% end
 
-
+% err
+% C = A(mini(1)) 
+% sigma = A(mini(2))
 
 
 % =========================================================================
